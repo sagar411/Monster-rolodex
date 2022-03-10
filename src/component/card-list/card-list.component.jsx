3a3bf6473665fd6345d582266ba3ae1615c6f68e
sagar411 <sagar.sunar411@gmail.com>
2022-03-10 18:30:25 +0545

    using simle api that provide photos-diffrent photo with different key

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -5,7 +5,7 @@ import { Card } from "../card/card.component";
 export const CardList =(props)=>{ 
     console.log(props)
 return (<div className="card-list">
-    {props.monstors.map(monstor => <h1 key={monstor.id}>{monstor.name} </h1>)
+    {props.monstors.map(monster => <Card key={monster.id} monster={monster}></Card> )
   }</div>)
 }
 
--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import './card.styles.css'
 export const Card = (props)=>{
  return(
- <div>
+ <div className="card-container">
+     {/* urlparameter */}
+     {/* <img alt="monsters" src={`https://robohash.org/${props.monster.id}?set=set2&size=&size=180x180`}></img> */}
+     <img alt="monsters" src={`https://robohash.org/${props.monster.id}&size=180x180`}></img>
      <h1 >{props.monster.name}</h1>
+     <p>{props.monster.email}</p>
+     {/* <p>{props.monster.company.name}</p> */}
  </div>)
 }
